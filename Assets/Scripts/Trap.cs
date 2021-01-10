117c964e88a7cfdaacf9caf2f0881c947d0240e0
Singunsu <76992660+Singunsu@users.noreply.github.com>
2021-01-10 21:47:41 +0900

    asd

--- Assets/Scripts/Trap.cs
+++ Assets/Scripts/Trap.cs
@@ -46,11 +46,11 @@ public class Trap : MonoBehaviour
         // X축으로 이동하는 트랩 설정
         if (Check == true)
         {
-            if (transform.position.x > StartPoint && transform.position.x < EndPoint && Ham == true && timerx < 0)
+            if (transform.position.x < StartPoint && Ham == true && timerx < 0)
             {
                 gameObject.transform.position = new Vector2(transform.position.x + 0.02f, transform.position.y);
                 // X는 -축이 왼쪽 , +축이 오른쪽인데, 현재 StartPoint가 5라고 가정을 해보자
-                // Dir = false가 되었을 때는 오른쪽으로 게속 이동을 할 것이다
+                // Dir = false가 되었을 때는 왼쪽으로 게속 이동을 할 것이다
                 // 그러면 x 좌표가 5를 넘어서는 순간 StartPoint보다 크다고 인식을 하여
                 // Dir = true로 설정 시킨다.
                 Dir = true;
@@ -59,14 +59,14 @@ public class Trap : MonoBehaviour
             }
             else if (transform.position.x > EndPoint && Ham == true && timerx < 0)
             {
-                gameObject.transform.position = new Vector2(transform.position.x+0.02f, transform.position.y);
+                gameObject.transform.position = new Vector2(transform.position.x-0.02f, transform.position.y);
                 // Dir = true가 되었을 때는 왼쪽으로 이동을 할 것이다
                 // 근데 x축으로 왼쪽으로 이동하는건 음수로 넘어간다 / EndPoint는 -5라고 가정한다
                 // 트랩이 -5를 넘어서 가게 되는 순간에 EndPoint보다 현재 트랩의 X축 좌표가 작다고 판정하여
                 // Dir = false로 설정 시킨다.
                 Dir = false;
                 timerx = timerxx;
-                PlayerFilp.flipX = true;
+                PlayerFilp.flipX = false;
 
             }
         }
@@ -81,7 +81,6 @@ public class Trap : MonoBehaviour
                 // Dir = true로 설정 시킨다.
                 Dir = false;
                 timerx = timerxx;
-                PlayerFilp.flipX = false;
             }
             else if (transform.position.x > EndPoint && Ham == false && timerx < 0)
             {
@@ -92,7 +91,6 @@ public class Trap : MonoBehaviour
                 // Dir = false로 설정 시킨다.
                 Dir = true;
                 timerx = timerxx;
-                PlayerFilp.flipX = true;
 
             }
         }
@@ -141,11 +139,13 @@ public class Trap : MonoBehaviour
         {
             if (Dir == true)
             {
-                transform.Translate(Vector2.left * speed * Time.deltaTime);
+                PlayerFilp.flipX = true;
+                transform.Translate(Vector2.right * speed * Time.deltaTime);
             }
             else
             {
-                transform.Translate(Vector2.right * speed * Time.deltaTime);
+                PlayerFilp.flipX = false;
+                transform.Translate(Vector2.left * speed * Time.deltaTime);
             }
         }
         else if (Check == false && timery < 0)
