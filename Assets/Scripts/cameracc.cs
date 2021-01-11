0b10fc6f9c13922e6607163da0f4d824729110c2
csn <seoknyang@naver.com>
2021-01-11 16:46:08 +0900

    fist

--- Assets/Scripts/cameracc.cs
+++ Assets/Scripts/cameracc.cs
@@ -22,7 +22,7 @@ public class cameracc : MonoBehaviour
 
     // Start is called before the first frame update
 
-    
+
     void Awake()
     {
         playerc = GameObject.Find("Player").GetComponent<playerc>();
@@ -30,17 +30,14 @@ public class cameracc : MonoBehaviour
 
     public void Start()
     {
-        /*if (playerc.istag == true)
-        {
-            OnShakeCamera(1.0f,1.0f);
-        }*/
+
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(m_Player.transform.position.x, m_Player.transform.position.y,-10);
+        transform.position = new Vector3(m_Player.transform.position.x, m_Player.transform.position.y, -10);
 
         /*if (m_Player.transform.position.y >= 1.44f)
         {
@@ -54,7 +51,7 @@ public class cameracc : MonoBehaviour
 
     }
 
-    public void OnShakeCamera(float shakeTime=1.0f,float shakeIntensity = 0.05f)
+    public void OnShakeCamera(float shakeTime = 1.0f, float shakeIntensity = 0.05f)
     {
         this.shakeTime = shakeTime;
         this.shakeIntensity = shakeIntensity;
@@ -67,7 +64,7 @@ public class cameracc : MonoBehaviour
     {
         Vector3 startPosition = transform.position;
 
-        while(shakeTime > 0.0f)
+        while (shakeTime > 0.0f)
         {
             transform.position = startPosition + Random.insideUnitSphere * shakeIntensity;
 
@@ -79,22 +76,4 @@ public class cameracc : MonoBehaviour
         transform.position = startPosition;
     }
 
-    /*public IEnumerator ShakeByRotation()
-    {
-        Vector3 startRotation = transform.eulerAngles;
-
-        while (shakeTime > 0.0f)
-        {
-            float x = Random.Range(-1, 1f);
-            float y = 0;
-            float z = 0;
-            transform.rotation = Quaternion.Euler(startRotation + new Vector3(x, y, z) * shakeIntensity);
-
-            shakeTime -= Time.deltaTime;
-
-            yield return null;
-        }
-
-        transform.rotation = Quaternion.Euler(startRotation);
-    }*/
 }
