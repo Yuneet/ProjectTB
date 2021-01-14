9b70ef5ad07c2002b84d834e5b2b1b8c2b896547
Yuneet <76992544+Yuneet@users.noreply.github.com>
2021-01-14 15:29:39 +0900

    fist

--- Assets/Scripts/StoryCtrl.cs
+++ Assets/Scripts/StoryCtrl.cs
@@ -17,7 +17,7 @@ public class StoryCtrl : MonoBehaviour
     public GameObject gameObject8;
     public GameObject gameObject9;
     public GameObject gameObject10;
-
+    public Text text;
 
 
     void Awaek()
@@ -31,16 +31,66 @@ public class StoryCtrl : MonoBehaviour
         if (SeenNumners == 0)
         {
             gameObject1.SetActive(true);
+            gameObject10.SetActive(false);
+            text.text = "1";
         }else if (SeenNumners == 1)
         {
             gameObject1.SetActive(false);
             gameObject2.SetActive(true);
-        }else if (SeenNumners == 2)
+            text.text = "2";
+        }
+        else if (SeenNumners == 2)
         {
             gameObject2.SetActive(false);
             gameObject3.SetActive(true);
+            text.text = "3";
         }
-        
+        else if (SeenNumners == 3)
+        {
+            gameObject3.SetActive(false);
+            gameObject4.SetActive(true);
+            text.text = "4";
+        }
+        else if (SeenNumners == 4)
+        {
+            gameObject4.SetActive(false);
+            gameObject5.SetActive(true);
+            text.text = "5";
+        }
+        else if (SeenNumners == 5)
+        {
+            gameObject5.SetActive(false);
+            gameObject6.SetActive(true);
+            text.text = "6";
+        }
+        else if (SeenNumners == 6)
+        {
+            gameObject6.SetActive(false);
+            gameObject7.SetActive(true);
+            text.text = "7";
+        }
+        else if (SeenNumners == 7)
+        {
+            gameObject7.SetActive(false);
+            gameObject8.SetActive(true);
+            text.text = "8";
+        }
+        else if (SeenNumners == 8)
+        {
+            gameObject8.SetActive(false);
+            gameObject9.SetActive(true);
+            text.text = "9";
+        }
+        else if (SeenNumners == 9)
+        {
+            gameObject9.SetActive(false);
+            gameObject10.SetActive(true);
+            text.text = "10";
+        }else if (SeenNumners == 10)
+        {
+            SeenNumners = 0;
+        }
+
     }
 
     public void OnCrick()
