ace7d74549ff17c8a6d4ab66b3790c294dc92c2d
Yuneet <76992544+Yuneet@users.noreply.github.com>
2021-01-19 12:46:20 +0900

    fist

--- Assets/Scripts/endingcreditsCtrl.cs
+++ Assets/Scripts/endingcreditsCtrl.cs
@@ -8,7 +8,7 @@ public class endingcreditsCtrl : MonoBehaviour
 {
     public Text text;
     public Sprite[] sprite;
-    public string[] massage = {"프로그래밍 팀장 김규빈","프로그래밍 부원 안준수"};
+    private string[] massage = {"팀명 : MA - T","그래픽 팀장 이준","그래픽 부원 김도영","프로그래밍 팀장 김규빈","프로그래밍 팀원 안준수","프로그래밍 팀원 신건수","도움을 준 사람 김종상","총괄 대표 신건수","To be Continued"};
     public SpriteRenderer spriteRenderer;
     public int number;
     public float timer;
@@ -20,60 +20,90 @@ public class endingcreditsCtrl : MonoBehaviour
     {
         number = 0;
         timer = 0f;
-
+        //Debug.Log(massage[1]);
         StartCoroutine(Changes(spriteRenderer, sprite, text));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (number == 9)
+        {
+            StopCoroutine(Changes(spriteRenderer, sprite, text));
+            StartCoroutine(potal.LodeScenes(potal.potalnumber));
+            number++;
+        }
     }
 
     public void OnClick()
     {
         StopCoroutine(Changes(spriteRenderer, sprite, text));
-        //setting.SetActive(false);
-        //load.SetActive(true);
         StartCoroutine(potal.LodeScenes(potal.potalnumber));
     }
 
     public IEnumerator Changes(SpriteRenderer spriteRenderers, Sprite[] sprites, Text texts)
     {
-
         bool isShow = false;
+        
         while (!isShow && text.color.a >= 0)
         {
-            Debug.Log(1);
+            //Debug.Log(1);
             if (text.color.a >= 1)
             {
                 isShow = true;
             }
             if (!isShow)
             {
-                text.color = new Color(0, 0, 0, text.color.a + 0.05f);
-                //spriteRenderer.color = text.color;
+                text.text = massage[number];
+                text.color = new Color(0, 0, 0, text.color.a + 0.05f);              
                 spriteRenderer.color = new Color(1, 1, 1, spriteRenderer.color.a + 0.05f);
             }
             yield return new WaitForSeconds(0.1f);
         }
         while (isShow && text.color.a >= 0)
         {
-            Debug.Log(2);
+            //Debug.Log(2);
             text.color = new Color(0, 0, 0, text.color.a - 0.05f);
-            //spriteRenderer.color = text.color;
             spriteRenderer.color = new Color(1, 1, 1, spriteRenderer.color.a - 0.05f);
             yield return new WaitForSeconds(0.1f);
         }
-        number++;
+        text.color = new Color(0, 0, 0, 0);
+        spriteRenderer.color = new Color(1, 1, 1, 0);
+        number ++;
+        //Debug.Log("number : " + number);
         switch (number)
         {
-            case 1: StartCoroutine(Changes(spriteRenderer, sprite, text));
+            case 1:
+                StartCoroutine(Changes(spriteRenderer, sprite, text));
                 break;
-            case 2:                 
+            case 2:
+                StartCoroutine(Changes(spriteRenderer, sprite, text));
                 break;
-            default :
-                ; break;
+            case 3:
+                StartCoroutine(Changes(spriteRenderer, sprite, text));
+                break;
+            case 4:
+                StartCoroutine(Changes(spriteRenderer, sprite, text));
+                break;
+            case 5:
+                StartCoroutine(Changes(spriteRenderer, sprite, text));
+                break;
+            case 6:
+                StartCoroutine(Changes(spriteRenderer, sprite, text));
+                break;
+            case 7:
+                StartCoroutine(Changes(spriteRenderer, sprite, text));
+                break;
+            case 8:
+                StartCoroutine(Changes(spriteRenderer, sprite, text));
+                break;
+            /*case 9:
+                StartCoroutine(Changes(spriteRenderer, sprite, text));
+                break;
+            case 10:
+                break;*/
+            default :;
+                break;
         }
     }
 }
