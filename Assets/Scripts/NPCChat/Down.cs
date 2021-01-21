8e9d61cba1b3267fdbf4bca5663b9478dff428bf
GodUnKnown <Singunsu258@gmail.com>
2021-01-21 11:08:53 +0900

    132213

---
+++ Assets/Scripts/NPCChat/Down.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Down : MonoBehaviour
+{
+    public GameObject P;
+    public GameObject block;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (P.transform.position.x >= 1.25f)
+        {
+            block.SetActive(false);
+        }
+    }
+}
