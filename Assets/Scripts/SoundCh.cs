1cff6c778d1d4cd383cc10e707f3330139e1e692
csn <seoknyang@naver.com>
2021-01-25 11:28:34 +0900

    fist

---
+++ Assets/Scripts/SoundCh.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundCh : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip[] audioClips;
+    public GameObject P;
+    public bool istrue;
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask layer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        istrue = Physics2D.OverlapCircle(feetPos.position, checkRadius, layer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (P.transform.position.y > 55)
+        {
+            if (istrue == true)
+            {
+                audioSource.clip = audioClips[0];
+                audioSource.Play();
+            }
+        }
+        else if (P.transform.position.y > 131)
+        {
+            if (istrue == true)
+            {
+                audioSource.clip = audioClips[1];
+                audioSource.Play();
+            }
+        }
+        else if (P.transform.position.y > 220)
+        {
+            if (istrue == true)
+            {
+                audioSource.clip = audioClips[2];
+                audioSource.Play();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text text;
+    float sce;
+    int Min;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sce += Time.deltaTime;
+        text.text = string.Format("{0:D2} : {1:D2}", Min, (int)sce);
+       // text.text = Time.time.ToString("N2");
+        if((int)sce > 59)
+        { 
+            sce = 0;
+            Min++;
+        }
+      
+    }
+}
